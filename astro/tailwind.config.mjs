29e17c3edc6b93d67033acca41d312642cb0bd37
Stefano Tambellini <17550149+mangowh@users.noreply.github.com>
2024-11-27 01:00:34 +0100

    wip

--- astro/tailwind.config.mjs
+++ astro/tailwind.config.mjs
@@ -4,7 +4,6 @@ import colors from "tailwindcss/colors";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  darkMode: "class",
   theme: {
     screens: {
       xs: "380px",
